18f930f52cfacc144d58c403a18c56a4b21dc08b
smauricii <schamorro28@uan.edu.co>
2021-06-17 14:57:36 -0500

    prueba de main

--- app/src/main/java/com/example/lellarap/MainActivity.kt
+++ app/src/main/java/com/example/lellarap/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hola.text="HOla mundo asdasd"
+        hola.text="HOla mundo main"
 
     }
 
